53c38fdc263c345d68a9628db6c457af20ac9185
surfsky <surfsky@sina.com>
2020-05-14 15:00:04 +0800

    I hate mvvm

--- Todo/Views/TodoListView.xaml.cs
+++ Todo/Views/TodoListView.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System;
+using System.Diagnostics;
 
 namespace Todo.Views
 {
@@ -14,6 +17,14 @@ namespace Todo.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        public void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Handle click here.
+            //MessageBox.Avalonia.BaseWindows.
+
+            Trace.WriteLine("click");
         }
     }
 }
